e7e928cfbbb41ae6f62313a3360e8fac7751b366
Jhanett Vivian <152014155+DevJhanjhan@users.noreply.github.com>
2024-04-13 10:05:09 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+//Definindo variáveis
+var canvas, backgroundImage, brgImg, database, form, player, playerCount;
+
+function preload(){
+
+}
+
+function setup(){
+
+}
+
+function draw(){
+
+}
+
+//Ajustando a altura e largura automaticamente sempre que o tamanho da janela é alterado
+function windowResized() {
+    resizeCanvas(windowWidt,windowHeight);
+}
